ee5044afa51ca9f76ed0d10cc4314b7bafc536a8
Jeroen Ooms <jeroenooms@gmail.com>
2020-12-02 02:36:40 +0100

    add daily cron job

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
 set -e
-echo "Trying to set commit status for ${1}/commit/${2}"
-Rscript -e "commitstatus::gh_app_set_commit_status('${1}','${2}','${3}','${4}','${5}')"
+echo "Looking for new app installations...."
+Rscript -e "setupuniverse::setup_universes()"
 echo "Action complete!"
