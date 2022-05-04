171865de224820415cc8dee648a436f33ad55c34
Jeroen Ooms <jeroenooms@gmail.com>
2022-05-04 23:28:27 +0200

    add amices

--- R/setup.R
+++ R/setup.R
@@ -55,7 +55,7 @@ testusers <- c("azure", "bioconductor", "cboettig", "eddelbuettel", "hadley",
                "yihui", "test", "s-u", 'dmurdoch', 'yulab-smu', 'poissonconsulting',
                'paws-r', 'business-science', 'mangothecat','uscbiostats','usgs-r',
                'mazamascience','dynverse', 'reconhub', 'girke-lab', 'ohdsi','quanteda',
-               'rikenbit','stan-dev','storeylab','hansenlab')
+               'rikenbit','stan-dev','storeylab','hansenlab','amices')
 
 #' @export
 #' @rdname setup_universes
